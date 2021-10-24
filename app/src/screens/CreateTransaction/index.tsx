6222d3cef97a9bfe4e061e9bb047700f12276302
vanpo <ivan.posadas@moveassist.com>
2021-10-24 15:11:05 +0800

    initial create transaction screen

---
+++ app/src/screens/CreateTransaction/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Layout from '../../Layout';
+
+type CreateTransactionProps = {};
+
+function CreateTransaction({}: CreateTransactionProps) {
+  return (
+    <Layout>
+      <Text>Create Transaction</Text>
+    </Layout>
+  );
+}
+
+export default CreateTransaction;
