39f89e30defdc86a576029930c854cd97a25e740
vanpo <ivan.posadas@moveassist.com>
2021-10-24 15:11:30 +0800

    use function instead of es6

--- app/src/screens/Home/index.tsx
+++ app/src/screens/Home/index.tsx
@@ -1,11 +1,19 @@
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import React from 'react';
 import {Button, Text, View} from 'react-native';
 import Layout from '../../Layout';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+// type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
 
-const Home = ({navigation}: Props) => {
+type HomeProps = {
+  navigation: HomeScreenNavigationProp; //Props['navigation'];
+};
+
+function Home({navigation}: HomeProps) {
   const Logout = () => {
     navigation.reset({
       index: 0,
@@ -22,6 +30,6 @@ const Home = ({navigation}: Props) => {
       </View>
     </Layout>
   );
-};
+}
 
 export default Home;
